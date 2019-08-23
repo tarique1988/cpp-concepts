e44361ba8a7163a3e5533543792fed1f81fd691d
Tarique Ali Mirza <tarique1988@gmail.com>
2019-08-23 15:49:33 +0530

    initial commit - hello world!

---
+++ 000_Hello_World/main.cpp
@@ -0,0 +1,12 @@
+/*
+Prints Hello World! to the command prompt
+created by: Tarique Ali Mirza <tarique1988@gmail.com>
+date: 23rd August 2019
+*/
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
